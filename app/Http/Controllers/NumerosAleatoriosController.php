6371c7884e4ba615e05bfb780f8ca70df82ec66f
emi <emilianoblazco@gmail.com>
2023-04-16 20:38:20 -0300

    Actividad
    -Creacion de rutas y vistas para desarrollar el metodo de congruencias final
    -Verificar el algoritmo de congruencia

--- app/Http/Controllers/NumerosAleatoriosController.php
+++ app/Http/Controllers/NumerosAleatoriosController.php
@@ -43,19 +43,32 @@ class NumerosAleatoriosController extends Controller
     }
 
     public function congruencias(Request $request){
-//        $numeros = [];
-        $v1 = $request->input('semillav1');
-//        $cantidad = $request->input('cantidad');
-//        $a = $request->input('control');
-//        $c = $request->input('control2');
-//        $m = $request->input('control3');
+        $numeros = [];
+        $v1 = $request->input('semillavi');
+        $v2 = $request->input('semiillavik');
+        $cantidad = $request->input('cantidadCong');
+        $a = $request->input('constanteA');
+        $c = $request->input('constanteC');
+        $m = $request->input('constanteM');
         $numeros[0] = intval($v1);
-//
-//        for ($i=1; $i < $cantidad; $i++) {
-//            $v2 = ($a*$v1+$c)%$m;
-//            $numeros[] = $v2;
-//            $v1 = $v2;
+        $numeros[1] = intval($v2);
+
+        for ($i=1; $i < $cantidad; $i++) {
+            $v3 = (($a*$v1)+($c*$v2)) % $m;
+            $numeros[] = $v3;
+            $v1 = $v2;
+            $v2 = $v3;
+
+        }
+
+//        function congruenciaFundamental($v1, $v2, $a, $k, $m, $cantidad){
+//            $v = array($v1, $v2);
+//            for($i = 2; $i < $cantidad; $i++){
+//                $v[$i] = (($a * $v[$i-1]) + ($k * $v[$i-2])) % $m;
+//            }
+//            return $v;
 //        }
+        dd($numeros);
 
         return view('numerosAleatorios.congruencia', compact('numeros'));
     }
--- resources/views/numerosAleatorios/index.blade.php
+++ resources/views/numerosAleatorios/index.blade.php
@@ -23,48 +23,49 @@
 
     <hr style="height: 5px; background: #1a202c">
 
-    <h3>Método de Congruencia Fundamental</h3>
+    <div class="row">
+        <div class="col-6">
+            <h3 class="text-center" >Método de Congruencia Fundamental</h3>
 
-    <form action="{{ route('NA.congruencias') }}" method="POST">
-        @csrf
-        <label for="cantidad">Cantidad de n&uacute;meros a generar</label>
-        <input type="number" name="cantidad" id="cantidad" value="10">
-        <br>
-        <label for="semillav1">Primer Semilla(V1)</label>
-        <input type="number" name="semillav1" id="semillav1" value="300">
-        <br>
-        <label for="semillav2">Segunda Semilla(V2)</label>
-        <input type="number" name="semillav2" id="semillav2" value="400">
-        <br>
-        <label for="control">Parámetro de control(A)</label>
-        <input type="number" name="control" id="control" value="1000">
-        <br>
-        <button type="submit">Generar</button>
-    </form>
+            <p class="text-center h4">
+                La formula a utilizar para llevar a cabo la generación de números aleatorios es la siguiente:<br><br>
+                <strong>V<sub>i+1</sub> = (a*V<sub>i</sub> + c*V<sub>i-k</sub>) mod m</strong>
 
+            </p>
 
-    <h3>Método de Congruencia Fundamental</h3>
-
-    <form class="needs-validation" action="{{ route('NA.congruencias') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="cantidad">Cantidad de números a generar:</label>
-            <input type="number" class="form-control" name="cantidad" id="cantidad" value="10" required>
+            <form class="needs-validation mt-4" action="{{ route('NA.congruencias') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="cantidadCong">Cantidad de números a generar:</label>
+                    <input type="number" class="form-control w-50" name="cantidadCong" id="cantidadCong" value="10" required>
+                </div>
+                <div class="form-group">
+                    <label for="semillavi">Semilla(V<sub>i</sub>):</label>
+                    <input type="number" class="form-control w-50" name="semillavi" id="semillavi" value="300" required>
+                </div>
+                <div class="form-group">
+                    <label for="semiillavik">Segunda semilla(V<sub>i-k</sub>):</label>
+                    <input type="number" class="form-control w-50" name="semiillavik" id="semiillavik" value="1000" required>
+                </div>
+                <div class="form-group">
+                    <label for="constanteA">Primer Constante(a):</label>
+                    <input type="number" class="form-control w-50" name="constanteA" id="constanteA" value="400" required>
+                </div>
+                <div class="form-group">
+                    <label for="constanteC">Segunda Constante(c):</label>
+                    <input type="number" class="form-control w-50" name="constanteC" id="constanteC" value="1000" required>
+                </div>
+                <div class="form-group">
+                    <label for="constanteM">Cuarta Constante(m):</label>
+                    <input type="number" class="form-control w-50" name="constanteM" id="constanteM" value="1000" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Generar</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="semillav1">Primer Semilla(V1):</label>
-            <input type="number" class="form-control" name="semillav1" id="semillav1" value="300" required>
+        <div class="col-6">
+            <h3 class="text-center" >Resultados de la generación</h3>
         </div>
-        <div class="form-group">
-            <label for="semillav2">Segunda Semilla(V2):</label>
-            <input type="number" class="form-control" name="semillav2" id="semillav2" value="400" required>
-        </div>
-        <div class="form-group">
-            <label for="control">Parámetro de control(A):</label>
-            <input type="number" class="form-control" name="control" id="control" value="1000" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Generar</button>
-    </form>
+    </div>
 
 
 @endsection
